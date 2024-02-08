6c7ff10e7971e9d73010c54be78b8f5d3d23db61
Emmanuel <emmanguerrero613@gmail.com>
2024-02-08 13:02:43 -0600

    adding humhub modulr folder

---
+++ htdocs/protected/modules/custom_pages/vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => 'humhub/custom_pages',
+        'pretty_version' => 'v1.8.11',
+        'version' => '1.8.11.0',
+        'reference' => 'ead95255c91bcb0206a49b8acbe603766a1a1752',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '2amigos/yii2-tinymce-widget' => array(
+            'pretty_version' => '1.1.3',
+            'version' => '1.1.3.0',
+            'reference' => 'a58b7a59a1508f4251a8cea9e010d31c9733bde4',
+            'type' => 'yii2-extension',
+            'install_path' => __DIR__ . '/../2amigos/yii2-tinymce-widget',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'humhub/custom_pages' => array(
+            'pretty_version' => 'v1.8.11',
+            'version' => '1.8.11.0',
+            'reference' => 'ead95255c91bcb0206a49b8acbe603766a1a1752',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tinymce/tinymce' => array(
+            'pretty_version' => '6.4.0',
+            'version' => '6.4.0.0',
+            'reference' => '488a05ddf56e844e593a3fb7da68aa5fb41f01ac',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../tinymce/tinymce',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yiisoft/yii2' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
